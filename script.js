d1373a5a3aa48bf7e7557eb167ad4812ebfb94fa
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-04-05 18:00:35 -0400

    added div fields for calculator structure + buttons and a display

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
+const calculatorMain = document.querySelector("#calculatorMain");
+const calculatorDisplay = document.querySelector("#calculatorDisplay");
+const calculatorButtons = document.querySelector("#calculatorButtons");
+
+
+
 function add() {
     let num = [...arguments];
     return num.reduce((total, num) => total + num);
 }
-
 function subtract() {
     let num = [...arguments];
     return num.reduce((total, num) => total - num);
 }
-
 function multiply() {
     let num = [...arguments];
     return num.reduce((total, num) => total * num);
 }
-
 function divide() {
     let num = [...arguments];
     return num.reduce((total, num) => total / num);
 }
-
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
