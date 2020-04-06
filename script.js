0b4a5e3a2592166f67b14c82adf97eff60bcea56
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-04-06 19:26:46 -0400

    added number button and display functionality, including clear.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 const calcMain = document.getElementById("#calcMain");
 const calcDisplay = document.querySelector("#calcDisplay");
+const displayText = document.querySelector("#calcText");
+const textContent = [];
 const calcButtons = document.querySelector("#calcButtons");
-const displayText = document.querySelector("#displayText");
 const numButton = document.querySelectorAll(".numButton");
+const clearButton = document.querySelector("#clear");
 
+function populateDisplay(numbers) {
+    textContent.push(numbers);
+    updateCalcText(textContent);
+}
 
+function clearDisplay() {
+    textContent.length = 0;
+    updateCalcText(textContent);
+}
+
+function updateCalcText(text) {
+    displayText.textContent = text.join('');
+}
 
 function add() {
     let num = [...arguments];
@@ -25,3 +39,16 @@ function divide() {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
+function eventListeners() {
+    
+    for  (i = 0; i < numButton.length; i++) {
+        numButton[i].addEventListener('click', function() {
+            populateDisplay(this.textContent);
+        });
+    };
+    
+    clearButton.addEventListener('click', function () {
+        clearDisplay();
+    });
+} 
+eventListeners();
