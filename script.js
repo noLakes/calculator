af432f1a7e5a154c87a075c12fdae0817c9078d0
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-04-10 00:41:42 -0400

    build logic sequence and equation operator loop - need to fix math precedence though

--- script.js
+++ script.js
@@ -24,17 +24,50 @@ const logicStack = {
 const ls = logicStack;
 
 function operate() {
-
+    while (ls.equationBuild.includes('*')) {
+        let pos = ls.equationBuild.indexOf('*');
+        ls.equationBuild.splice(pos-1, 3, mathOp['*'](ls.equationBuild[pos-1], ls.equationBuild[pos+1]));
+        console.log(ls.equationBuild);
+    }
+    while (ls.equationBuild.includes('/')) {
+        let pos = ls.equationBuild.indexOf('/');
+        ls.equationBuild.splice(pos-1, 3, mathOp['/'](ls.equationBuild[pos-1], ls.equationBuild[pos+1]));
+        console.log(ls.equationBuild);
+    }
+    while (ls.equationBuild.includes('+')) {
+        let pos = ls.equationBuild.indexOf('+');
+        ls.equationBuild.splice(pos-1, 3, mathOp['+'](ls.equationBuild[pos-1], ls.equationBuild[pos+1]));
+        console.log(ls.equationBuild);
+    }
+    while (ls.equationBuild.includes('-')) {
+        let pos = ls.equationBuild.indexOf('-');
+        ls.equationBuild.splice(pos-1, 3, mathOp['-'](ls.equationBuild[pos-1], ls.equationBuild[pos+1]));
+        console.log(ls.equationBuild);
+    }
+    console.log(ls.equationBuild);
     }
     
 function addNum(num) {
-    //addnum code
+    ls.newNumbers.push(num);
+    equationTextContent.push(num);
     updateDisplay();
 }
 
 function addOp(op) {
-//addOp code
+    ls.equationBuild.push(Number(ls.newNumbers.join('')));
+    ls.newNumbers.length = 0;
+    ls.equationBuild.push(op);
+    equationTextContent.push(op);
     updateDisplay();
+}
+
+function equals() {
+    ls.equationBuild.push(newNumbers.join(''));
+    ls.newNumbers.length = 0;
+
+    updateDisplay();
+
+
 }
 
 function clearDisplay() {
